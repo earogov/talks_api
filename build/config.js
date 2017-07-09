a7795c86fd8ebad9d6007901dad1e15b379c7fbc
Eugene Rogov <earogov@gmail.com>
2017-07-09 15:38:02 +0300

    Remove build dir from git

--- build/config.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.isProd = process.env.NODE_ENV === "production";
-exports.port = process.env.PORT || 8080;
-//# sourceMappingURL=config.js.map
